7b680970665d2ba316d67614b518adf196b0f5ce
Dhruvgabani7 <dhruvgabani45@gmail.com>
2025-10-04 15:54:12 +0530

    added routes

--- expense-backend/src/routes/users.js
+++ expense-backend/src/routes/users.js
@@ -7,6 +7,12 @@ const router = express.Router();
 router.use(authenticateToken);
 router.use(requireRole(['Admin']));
 
+router.get('/', getUsers);
+router.post('/', createUser);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
+
+module.exports = router;
 
 
 
